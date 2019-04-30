5fa4784dbca1d5bc4d94f43668441b57a888e296
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-04-30 09:39:49 -0300

    Aula 7 - Ordenação de listas

--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -7,7 +7,7 @@ package br.com.bytebank.banco.modelo;
  *
  */
 
-public abstract class Conta {
+public abstract class Conta extends Object implements Comparable<Conta> {
 
 	protected double saldo;
 	private int agencia;
@@ -92,7 +92,7 @@ public abstract class Conta {
 
 	@Override
 	public String toString() {
-		return "Numero: " + this.numero + ", Agencia:" + this.agencia;
+		return "Numero: " + this.numero + ", Agencia: " + this.agencia + ", Saldo:" + this.saldo;
 	}
 
 	@Override
@@ -109,6 +109,11 @@ public abstract class Conta {
 		}
 
 		return true;
+	}
+
+	@Override
+	public int compareTo(Conta outro) {
+		return Double.compare(this.saldo, outro.saldo);
 	}
 
 }
